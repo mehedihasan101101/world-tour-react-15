b48342a5888589d3716716f21415ac5d46cab6a7
mehedihasan101101 <sojibkhan39621@gmail.com>
2025-04-23 13:29:55 +0100

    details added on the card

--- src/components/individual-country/country.jsx
+++ src/components/individual-country/country.jsx
@@ -2,13 +2,16 @@ import './country.css'
 
 export default function Country({country}){
     console.log(country)
-const {name,flags} = country;
+const {name,flags,capital,area,continents} = country;
        return(
   
         <>
         <div className="individual-container">            
             <img src={flags.png} alt="" />
-            <h1>name: {name.common} </h1>
+            <p>Name: {name.common} </p>
+            <p>Capital: {capital} </p>
+            <p>Area: {area} km²</p>
+            <p>Continent: {continents} </p>
 
         </div>
         </>
