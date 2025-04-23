14b30487b208be9aeec90d80baf2a4943ff988dc
mehedihasan101101 <sojibkhan39621@gmail.com>
2025-04-23 12:51:44 +0100

    the site has been deployed to git pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/world-tour-react-project-15/',
+  base: '/world-tour-react-15',
   plugins: [react()],
 })
