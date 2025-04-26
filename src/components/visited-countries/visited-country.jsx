6ba04d33c1efba3791627cf47e7f5194bae9adf6
mehedihasan101101 <sojibkhan39621@gmail.com>
2025-04-26 11:43:51 +0100

    delete btn color updated for dark mode

--- src/components/visited-countries/visited-country.jsx
+++ src/components/visited-countries/visited-country.jsx
@@ -14,7 +14,7 @@ export default function VisitedList({ totalVisitedCountry, deleteBtnEventHandler
             <div className='single-liked-country' key={individual.name.common}>
               <div className='btn-container'>
                 <button className='btn-delete' onClick={() => deleteBtnEventHandler(individual.name.common)}><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16">
-                  <path fill='#213547' d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708" />
+                  <path fill='' d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708" />
                 </svg></button>
               </div>
 
